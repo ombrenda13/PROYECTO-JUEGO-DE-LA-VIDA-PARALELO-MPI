3938c44b3cbec22047360bc97577ab93dcbb89cc
ombrenda13 <36515687+ombrenda13@users.noreply.github.com>
2019-11-27 00:56:53 -0600

    FINAL

--- proyecto_final.c
+++ proyecto_final.c
@@ -4,23 +4,23 @@
 #include <mpi.h>
 
 int main(int argc, char **argv){
-  int i,j,np,pid,iteraciones,k,n=3;
+  int i,j,np,pid,iteraciones,k,n=3,vecinoDerecho,vecinoIzquierdo,VVivos;
   int filas=7;
-
-  int envio[4];
-  int arregloA[np];
   double Ti,Tf;
 
-  int columnas;
+  int columnas=5;
 
 
 
-int matrizA[filas][3];
+int matrizA[filas][columnas];
+int matrizB[filas][columnas];
+
+
   MPI_Status stat;
   MPI_Init(&argc,&argv);
   MPI_Comm_rank(MPI_COMM_WORLD,&pid);
   MPI_Comm_size(MPI_COMM_WORLD,&np);
-  int fila_actual[2],fila_abajo[2],fila_arriba[2],abajo=pid+1,arriba=pid-1;
+  int fila_actual[columnas-1],fila_actualizada[columnas-1],fila_abajo[columnas-1],fila_arriba[columnas-1],abajo=pid+1,arriba=pid-1;
 //iniciando tablero
 if(pid==0){
   printf("numero de procesos totales:%d\n",np );
@@ -30,166 +30,408 @@ if(pid==0){
 
    printf("\n");
         	for(i=0;i<filas;++i){
-        		for(j=0; j<n;++j){
+        		for(j=0; j<columnas;++j){
         			matrizA[i][j]=0+rand()%2;
         		    }
         	     }
             //IMPRIMIR MATRIZ tablero
+
+            printf("tablero orginal\n");
             for(i=0;i<filas;i++){
-                  for(j=0;j<n;j++){
+                  for(j=0;j<columnas;j++){
                      printf("%i",matrizA[i][j]);
                   }
                printf("\n");
 
              }
 
-    MPI_Scatter(&matrizA,3,MPI_INT,&fila_actual,3,MPI_INT,0,MPI_COMM_WORLD);//podria el buffer ir sin &
+    MPI_Scatter(&matrizA,columnas,MPI_INT,&fila_actual,columnas,MPI_INT,0,MPI_COMM_WORLD);//podria el buffer ir sin &
 
-    for(int c=0;c<3;c++){
+  /*  for(int c=0;c<3;c++){
         printf("soy el proceso:%d y mi elemento es:%d\n",pid,fila_actual[c]);
 
         }
-
+*/
 
       //COMUNICACIÓN DE PROCESOS
-      MPI_Sendrecv(&fila_actual, 3 , MPI_INT,abajo, 0 ,&fila_abajo, 3 , MPI_INT,abajo, 0,MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-      MPI_Sendrecv(&fila_actual, 3 , MPI_INT,np-1, 0 ,&fila_arriba, 3 , MPI_INT,arriba, 0,MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      MPI_Sendrecv(&fila_actual, columnas , MPI_INT,abajo, 0 ,&fila_abajo, columnas , MPI_INT,abajo, 0,MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      MPI_Sendrecv(&fila_actual, columnas , MPI_INT,np-1, 0 ,&fila_arriba, columnas , MPI_INT,arriba, 0,MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
         int k;
-        for(k=0;k<3;k++){
+        for(k=0;k<columnas;k++){
           printf("ROOT recibi...%d\n",fila_abajo[k]);
      }
-     for(k=0;k<3;k++){
+     for(k=0;k<columnas;k++){
        printf("ROOT recibi...%d\n",fila_arriba[k]);
   }
-  //    MPI_Send(&fila_actual,3,MPI_INT,arriba,0,MPI_COMM_WORLD);
 
-      //  MPI_Send(&fila_actual,3,MPI_INT,2,0,MPI_COMM_WORLD);
+  //Vecindad usada=Moore
 
-}else{
-      MPI_Scatter(&matrizA,3,MPI_INT,&fila_actual,3,MPI_INT,0,MPI_COMM_WORLD);//podria el buffer ir sin &
-      int a;
-      int fila_abajo[2],fila_arriba[2];
-      for(int a=0;a<3;a++){
-          printf("soy el proceso:%d y mi elemento es:%d\n",pid,fila_actual[a]);
+
+
+
+for(i=0;i<columnas;i++){
+  vecinoDerecho=i+1;
+    vecinoIzquierdo=i-1;
+    //revisa a su derecha (borde)
+      if (vecinoDerecho>=columnas){
+        vecinoDerecho=0;
+      }
+    //revisa a su izquierda (borde)
+      if (vecinoDerecho<0){
+        vecinoDerecho=columnas-1;
+      }
+    //Contador de vecinos
+      int VVivos=0;
+      int Vmuertos=0;
+      if(fila_arriba[vecinoIzquierdo]==1){
+        VVivos++;
+      }
+      else{Vmuertos++;
+      }
+      if(fila_arriba[i]==1){
+        VVivos++;
+      }else{Vmuertos++;
+      }
+      if(fila_arriba[vecinoDerecho]==1){
+        VVivos++;
+      }
+      else{Vmuertos++;
+      }
+      if(fila_actual[vecinoIzquierdo]==1){
+        VVivos++;
+      }
+      else{Vmuertos++;
+      }
+      if(fila_actual[vecinoDerecho]==1){
+        VVivos++;
+      }
+      else{Vmuertos++;
+      }
+      if(fila_abajo[vecinoIzquierdo]==1){
+        VVivos++;
+      }
+      else{Vmuertos++;
+      }
+      if(fila_abajo[i]==1){
+        VVivos++;
+      }
+      else{Vmuertos++;
+      }
+      if(fila_abajo[vecinoDerecho]==1){
+        VVivos++;
+      }
+      else{Vmuertos++;
+      }
+     printf("\n");
+    /*  printf("PID:%d y VIVOS:%d\n",pid,VVivos);
+      printf("PID:%d y MUERTOS:%d\n",pid,Vmuertos);
+      printf("\n");
+*/
+      if(fila_actual[i]==1){
+          if(VVivos<2){
+            fila_actualizada[i]=0;//muere de soledad
+
+          }else{
+            fila_actualizada[i]=1;
+
+          }
+          if(VVivos==2 || VVivos==3){
+            fila_actualizada[i]=0;//muere de soledad
+
+          }else{
+            fila_actualizada[i]=1;
+
           }
 
-      if(pid!=np-1){
-        printf("\n");
-        MPI_Sendrecv(&fila_actual, 3 , MPI_INT,arriba, 0 ,&fila_arriba, 3 , MPI_INT,arriba, 0,MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Sendrecv(&fila_actual, 3 , MPI_INT,abajo, 0 ,&fila_abajo, 3 , MPI_INT,abajo, 0,MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+          if(VVivos>3){
+            fila_actualizada[i]=0;//muere de sobrepoblación
 
-      //  MPI_Recv(&fila_arriba,3,MPI_INT,arriba,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-        //MPI_Send(&fila_actual,3,MPI_INT,0,0,MPI_COMM_WORLD);
-        for(a=0;a<3;a++){
+          }else{
+            fila_actualizada[i]=1;
 
-        printf("SOY EL PROCESO :%d y recibi la fila de arriba:%d\n",pid,fila_arriba[a]);
-      //  printf("recibi fila de abajo:%d y pid:%d\n",fila_abajo[a],pid);
+          }
 
       }
-      for(a=0;a<3;a++){
+      if(fila_actual[i]==0){
+        if(VVivos>=3){
+          fila_actualizada[i]=1;//revive
+        }else{
+          fila_actualizada[i]=0;
+
+        }
+
+      }
+
+
+
+}
+
+
+for(k=0;k<columnas;k++){
+//  printf("ROOT matriz actualizada...%d\n",fila_actualizada[k]);
+}
+
+MPI_Gather(&fila_actualizada,columnas,MPI_INT,&matrizB,columnas,MPI_INT,0,MPI_COMM_WORLD);
+printf("Tablero actualizado\n");
+for(i=0;i<filas;i++){
+      for(j=0;j<columnas;j++){
+         printf("%d",matrizB[i][j]);
+      }
+   printf("\n");
+
+ }
+
+}else{
+      MPI_Scatter(&matrizA,columnas,MPI_INT,&fila_actual,columnas,MPI_INT,0,MPI_COMM_WORLD);//podria el buffer ir sin &
+      int a;
+      int fila_abajo[columnas],fila_arriba[columnas];
+    /*  for(int a=0;a<3;a++){
+          printf("soy el proceso:%d y mi elemento es:%d\n",pid,fila_actual[a]);
+          }
+*/
+      if(pid!=np-1){
+        printf("\n");
+        MPI_Sendrecv(&fila_actual, columnas , MPI_INT,arriba, 0 ,&fila_arriba, columnas , MPI_INT,arriba, 0,MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Sendrecv(&fila_actual, columnas , MPI_INT,abajo, 0 ,&fila_abajo, columnas , MPI_INT,abajo, 0,MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+
+  /*     for(a=0;a<columnas;a++){
+
+        printf("SOY EL PROCESO :%d y recibi la fila de arriba:%d\n",pid,fila_arriba[a]);
+                        }
+
+      for(a=0;a<columnas;a++){
 
       printf("SOY EL PROCESO :%d y recibi la fila de abajo:%d\n",pid,fila_abajo[a]);
 
 
-    }
+    }*/
+for(int i=0;i<columnas;i++){
+    vecinoDerecho=i+1;
+      vecinoIzquierdo=i-1;
+      //revisa a su derecha (borde)
+        if (vecinoDerecho>=columnas){
+          vecinoDerecho=0;
+        }
+      //revisa a su izquierda (borde)
+        if (vecinoDerecho<0){
+          vecinoDerecho=columnas-1;
+        }
+      //Contador de vecinos
+        int VVivos=0;
+        int Vmuertos=0;
+        if(fila_arriba[vecinoIzquierdo]==1){
+          VVivos++;
+        }
+        else{Vmuertos++;
+        }
+        if(fila_arriba[i]==1){
+          VVivos++;
+        }else{Vmuertos++;
+        }
+        if(fila_arriba[vecinoDerecho]==1){
+          VVivos++;
+        }
+        else{Vmuertos++;
+        }
+        if(fila_actual[vecinoIzquierdo]==1){
+          VVivos++;
+        }
+        else{Vmuertos++;
+        }
+        if(fila_actual[vecinoDerecho]==1){
+          VVivos++;
+        }
+        else{Vmuertos++;
+        }
+        if(fila_abajo[vecinoIzquierdo]==1){
+          VVivos++;
+        }
+        else{Vmuertos++;
+        }
+        if(fila_abajo[i]==1){
+          VVivos++;
+        }
+        else{Vmuertos++;
+        }
+        if(fila_abajo[vecinoDerecho]==1){
+          VVivos++;
+        }
+        else{Vmuertos++;
+        }
+    /*    printf("\n");
+        printf("PID:%d y VIVOS:%d\n",pid,VVivos);
+        printf("PID:%d y MUERTOS:%d\n",pid,Vmuertos);
+        printf("\n");*/
 
+        if(fila_actual[i]==1){
+            if(VVivos<2){
+              fila_actualizada[i]=0;//muere de soledad
+
+            }else{
+              fila_actualizada[i]=1;
+
+            }
+            if(VVivos==2 || VVivos==3){
+              fila_actualizada[i]=0;//muere de soledad
+
+            }else{
+              fila_actualizada[i]=1;
+
+            }
+
+            if(VVivos>3){
+              fila_actualizada[i]=0;//muere de sobrepoblación
+
+            }else{
+              fila_actualizada[i]=1;
+
+            }
+
+        }
+        if(fila_actual[i]==0){
+          if(VVivos>=3){
+            fila_actualizada[i]=1;//revive
+          }else{
+            fila_actualizada[i]=0;
+
+          }
+
+        }
+
+
+
+
+
+}
+
+for(int k=0;k<columnas;k++){
+//  printf("pid:%d matriz actualizada...%d\n",pid,fila_actualizada[k]);
+}
+MPI_Gather(&fila_actualizada,columnas,MPI_INT,&matrizB,columnas,MPI_INT,0,MPI_COMM_WORLD);
 
       }
 
       if(pid==np-1){
         printf("\n");
-        MPI_Sendrecv(&fila_actual, 3 , MPI_INT,0, 0 ,&fila_arriba, 3 , MPI_INT,0, 0,MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Sendrecv(&fila_actual, 3 , MPI_INT,arriba, 0 ,&fila_abajo, 3 , MPI_INT,arriba, 0,MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Sendrecv(&fila_actual, columnas , MPI_INT,0, 0 ,&fila_arriba, columnas , MPI_INT,0, 0,MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Sendrecv(&fila_actual, columnas, MPI_INT,arriba, 0 ,&fila_abajo, columnas , MPI_INT,arriba, 0,MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-        for(a=0;a<3;a++){
+     for(a=0;a<columnas;a++){
 
         printf("SOY EL PROCESO :%d y recibi la fila de abajo:%d\n",pid,fila_arriba[a]);
 
     }
-    for(a=0;a<3;a++){
+    for(a=0;a<columnas;a++){
 
     printf("SOY EL PROCESO :%d y recibi la fila de arriba:%d\n",pid,fila_abajo[a]);
 
 }
 
 
-  }
+for(int i=0;i<columnas;i++){
+vecinoDerecho=i+1;
+  vecinoIzquierdo=i-1;
+  //revisa a su derecha (borde)
+    if (vecinoDerecho>=columnas){
+      vecinoDerecho=0;
+    }
+  //revisa a su izquierda (borde)
+    if (vecinoDerecho<0){
+      vecinoDerecho=columnas-1;
+    }
+  //Contador de vecinos
+    int VVivos=0;
+    int Vmuertos=0;
 
-  /*    for(int c=0;c<3;c++){
-          printf("soy el proceso:%d y mi elemento esss:%d\n",pid,fila_actual[c]);
-        }*/
-//1 envie al 0 y al 2
-    //   MPI_Recv(&fila_arriba, 3, MPI_INT, arriba, 0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);//recibe de proceso 0/root      //  MPI_Send(&fila_actual,3,MPI_INT,arriba,0,MPI_COMM_WORLD);
+    if(fila_arriba[vecinoIzquierdo]==1){
+      VVivos++;
+    }
+    else{Vmuertos++;
+    }
+    if(fila_arriba[i]==1){
+      VVivos++;
+    }else{Vmuertos++;
+    }
+    if(fila_arriba[vecinoDerecho]==1){
+      VVivos++;
+    }
+    else{Vmuertos++;
+    }
+    if(fila_actual[vecinoIzquierdo]==1){
+      VVivos++;
+    }
+    else{Vmuertos++;
+    }
+    if(fila_actual[vecinoDerecho]==1){
+      VVivos++;
+    }
+    else{Vmuertos++;
+    }
+    if(fila_abajo[vecinoIzquierdo]==1){
+      VVivos++;
+    }
+    else{Vmuertos++;
+    }
+    if(fila_abajo[i]==1){
+      VVivos++;
+    }
+    else{Vmuertos++;
+    }
+    if(fila_abajo[vecinoDerecho]==1){
+      VVivos++;
+    }
+    else{Vmuertos++;
+    }
+/*    printf("\n");
+    printf("PID:%d y VIVOS:%d\n",pid,VVivos);
+    printf("PID:%d y MUERTOS:%d\n",pid,Vmuertos);
+    printf("\n");*/
 
-        //envia al proceso anterior
-    /*    MPI_Send(&fila_actual,3,MPI_INT,arriba,pid-1,MPI_COMM_WORLD);
-        //recibe del proceso siguiente
-        MPI_Recv(&fila_abajo,3,MPI_INT,abajo,pid+1,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-        //envia al proceso siguiente
-        MPI_Send(&fila_actual,3,MPI_INT,abajo,pid+1,MPI_COMM_WORLD);
-        //recibe del proceso anterior
-        MPI_Recv(&fila_arriba,3,MPI_INT,arriba,pid-1,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-*/
-      //  MPI_Send(&fila_actual,3,MPI_INT,arriba,0,MPI_COMM_WORLD);//envia su fila actual del procesador
+  if(fila_actual[i]==1){
+        if(VVivos<2){
+          fila_actualizada[i]=0;//muere de soledad
 
+        }
 
-    //    MPI_Recv(&fila_abajo, 3, MPI_INT, abajo, 0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+        if(VVivos==2 || VVivos==3){
+          fila_actualizada[i]=0;//muere de soledad
 
+        }
 
-//  MPI_Recv(&fila_abajo,3,MPI_INT,vecino_abajo,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-//  MPI_Send(&fila_actual,3,MPI_INT,vecino_abajo,0,MPI_COMM_WORLD);
-//  MPI_Recv(&fila_arriba,3,MPI_INT,vecino_arriba,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+        if(VVivos>3){
+          fila_actualizada[i]=0;//muere de sobrepoblación
 
+        }
 
+    }
+    if(fila_actual[i]==0){
+      if(VVivos>=3){
+        fila_actualizada[i]=1;//revive
+      }
 
-
-  // imprimir_tablero(matriz);
-      //MPI_Barrier(MPI_COMM_WORLD);
-        //MPI_Gather(&recibir, 10, MPI_INT, &regreso,10, MPI_INT, 0, MPI_COMM_WORLD);
-
-
-            /*  for (int i = 0; i < 10; i++) {
-                  printf("\n%d", regreso[i]);
-              }
-*/
-
-    //  Ti=MPI_Wtime();
-      //for(k=0;k<=5;k++){
-        //SCATTER
-       // printf("P0--> Enviando datos a P%d:",pid);
-
-	//printf("buffercito..\n");
-	//printf("%d\n",buffer1[pid]);
-//}
+    }
 
 
 
 
-
-
-
-//int a=MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD,
-     //        MPI_STATUS_IGNORE);
-	//printf("%d\n",buffer1[pid]);
-	//printf("%d",number);
-	//printf("%d\n",pid);
-	//printf("soy difente de p0");
-
-   //Creando tablero
-
-
-
-/*
-
-
-    Tf=MPI_Wtime();
-
-   */
-	//fin else
+}
+for(int k=0;k<columnas;k++){
+  //printf("pid:%d matriz actualizada...%d\n",pid,fila_actualizada[k]);
+}
+MPI_Gather(&fila_actualizada,columnas,MPI_INT,&matrizB,columnas,MPI_INT,0,MPI_COMM_WORLD);
 }
 //printf("Tiempo de ejecucion: %f",Ti-Tf);
+
+}
+
+
+
 MPI_Finalize();
+//imprimir matriz actualizada
+//IMPRIMIR MATRIZ tablero
+
 return 0;
 
 }
